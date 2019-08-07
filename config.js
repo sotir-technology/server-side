6b7dc09954ad04a7e810ac6916684cf19554fa0c
rscbyte <nusktecsoft@gmail.com>
2019-08-07 16:32:44 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,2 @@
+//define constant variables here
+export const LOCAL_STORAGE = 'userAC'; //Local name for logged in user
---
+++ src/views/pages/Login.vue
@@ -0,0 +1,111 @@
+<!-- =========================================================================================
+    File Name: Login.vue
+    Description: Login Page
+    ----------------------------------------------------------------------------------------
+    Item Name: Vuesax Admin - VueJS Dashboard Admin Template
+      Author: Pixinvent
+    Author URL: http://www.themeforest.net/user/pixinvent
+========================================================================================== -->
+
+
+<template>
+    <div class="h-screen flex w-full bg-img" id="page-login">
+        <div class="vx-col sm:w-1/2 md:w-1/2 lg:w-3/4 xl:w-3/5 mx-auto self-center">
+            <vx-card>
+                <div slot="no-body" class="full-page-bg-color">
+                    <div class="vx-row">
+                        <div class="vx-col hidden sm:hidden md:hidden lg:block lg:w-1/2 mx-auto self-center">
+                            <img src="@/assets/images/pages/login.png" alt="login" class="mx-auto">
+                        </div>
+                        <div class="vx-col sm:w-full md:w-full lg:w-1/2 mx-auto self-center  d-theme-dark-bg">
+                            <div class="p-8">
+                                <div class="vx-card__title mb-8">
+                                    <h4 class="mb-4">Login</h4>
+                                    <p>Welcome back, please login to your account.</p>
+                                </div>
+                                <vs-input
+                                    v-validate="'required|email|min:3'"
+                                    data-vv-validate-on="blur"
+                                    name="email"
+                                    icon="icon icon-user"
+                                    icon-pack="feather"
+                                    label-placeholder="Email"
+                                    v-model="email"
+                                    class="w-full no-icon-border"/>
+                                <span class="text-danger text-sm">{{ errors.first('email') }}</span>
+
+                                <vs-input
+                                    data-vv-validate-on="blur"
+                                    v-validate="'required|min:6|max:10'"
+                                    type="password"
+                                    name="password"
+                                    icon="icon icon-lock"
+                                    icon-pack="feather"
+                                    label-placeholder="Password"
+                                    v-model="password"
+                                    class="w-full mt-6 no-icon-border" />
+                                <span class="text-danger text-sm">{{ errors.first('password') }}</span>
+
+                                <div class="flex flex-wrap justify-between my-5">
+                                    <vs-checkbox v-model="checkbox_remember_me" class="mb-3">Remember Me</vs-checkbox>
+                                    <router-link to="/pages/forgot-password">Forgot Password?</router-link>
+                                </div>
+                                <vs-button  type="border" @click="registerUser">Contact Dev</vs-button>
+                                <vs-button class="float-right" :disabled="!validateForm" @click="login">Login</vs-button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </vx-card>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import {LOCAL_STORAGE} from "../../../config";
+
+export default {
+    data() {
+        return {
+            email: 'demo@demo.com',
+            password: 'demodemo',
+            checkbox_remember_me: false
+        }
+    },
+    computed: {
+        validateForm() {
+            return !this.errors.any() && this.email !== '' && this.password !== '';
+        },
+    },
+    methods: {
+        login() {
+            localStorage.setItem(LOCAL_STORAGE, JSON.stringify({name: 'Onuche', role: 'admin'}));
+            this.$router.push(this.$router.currentRoute.query.to || '/')
+        },
+        contactDeveloperUser() {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#page-login {
+    .social-login {
+        .bg-facebook {
+          background-color: #1551b1;
+        }
+        .bg-twitter {
+          background-color: #00aaff;
+        }
+        .bg-google {
+          background-color: #1551b1;
+        }
+        .bg-github {
+          background-color: #333;
+        }
+    }
+}
+</style>
